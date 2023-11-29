c28a5b53a8f487674afd29bfe6e6561b53bbbcab
Gatovsky <101161255+hharieta@users.noreply.github.com>
2023-11-29 13:50:04 -0500

    Some tips for resources

--- services/aws/resources/rds/classSubnetGroup.py
+++ services/aws/resources/rds/classSubnetGroup.py
@@ -8,7 +8,7 @@ class SubnetGroupArgs:
                 project_name: str, 
                 region_short: str,
                 environment: str,
-                resource_name: str,
+                # resource_name: str, # Generalmente se crea el nombre del rucurso con los primeros parámetros
                 cloudbuddies: str = "True",
                 department: str = "nonprod",
                 opts: Optional[ResourceOptions] = None,
@@ -24,7 +24,7 @@ class SubnetGroupArgs:
         self.cloudbuddies: cloudbuddies
         self.department: department
         self.environment: environment
-        self.resource_name: resource_name
+        # self.resource_name: resource_name
         self.opts: opts
         self.description: description
         self.name: name
@@ -35,18 +35,25 @@ class SubnetGroupArgs:
 
 class SubnetGroupBuild(ComponentResource):
     def __init__(self,
+                nameResource: str,
                 args: SubnetGroupArgs,
                 opts: ResourceOptions = None
                 ):
+        super().__init__('custom:resource:SUBNETGROUP', nameResource, {}, opts) # inicializa la clase padre
     
-        if args.name is not None:
-             name = args.name
-        else:
-            name = f"subnetGroup-{args.project_name}-{args.environment}-{args.region_short}"
+        # if args.name is not None:
+        #      name = args.name
+        # else:
+        #     name = f"subnetGroup-{args.project_name}-{args.environment}-{args.region_short}"
+        
+        # refactorización del if anterior
+        if args.name is None:
+            args.name = f"subnetGroup-{args.project_name}-{args.environment}-{args.region_short}"
+        
 
         if args.tags is None:
             args.tags = {
-                'Name': name,
+                'Name': args.name,
                 'Project': args.project_name,
                 'Department': args.department,
                 'Cloudbuddies Archie': args.cloudbuddies,
@@ -54,10 +61,10 @@ class SubnetGroupBuild(ComponentResource):
                 }
 
         self.subnetGroup = aws.rds.SubnetGroup(
-                resource_name = args.resource_name,
-                opts = self.opts,
+                args.name,
+                opts = self.opts or ResourceOptions(parent=self),
                 description = args.description,
-                name = name,
+                name = args.name,
                 name_prefix = args.name_prefix,
                 subnet_ids = args.subnet_ids,
                 tags = args.tags,
